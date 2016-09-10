9aa8e598e4bbbf07962fcd8dfb5e718cc74b1278
Lior <Lior@Lior-PC>
2016-09-10 13:43:52 +0300

    run - fix

--- src/boot/Run.java
+++ src/boot/Run.java
@@ -6,6 +6,10 @@ import java.io.PrintWriter;
 import java.util.HashMap;
 
 import controller.Command;
+import controller.CommandsManager;
+import controller.Controller;
+import controller.MyController;
+import model.MyModel;
 import view.CLI;
 import view.MyView;
 
@@ -13,6 +17,8 @@ public class Run {
 
 	public static void main(String[] args) {
 		HashMap<String, Command> commands=new HashMap<>();
+		Model model=new MyModel(controller);
+		Controller controller=new MyController(view, model)
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in)); 
 		PrintWriter out = new PrintWriter(System.out);
 		CLI cli=new CLI(in,out,commands);
